91eafdcead02d9f8bc1d48d6ab60a42f5a91b2b8
aajay88287-maker <aajay88287@gmail.com>
2026-05-08 13:48:08 +0530

    commit push

---
+++ src/main/java/com/Temp/TemperatureCon.java
@@ -0,0 +1,45 @@
+package com.Temp;
+
+public class TemperatureCon {
+
+	
+	 class  ConvertFahrenheit{
+		 
+		 A = 1;
+		 
+		 (
+			 Celsisus = ( A * 9/5)+32;
+			 return A;
+			 
+		 )
+		 {
+			 kelvin = ( Celsius = 273.15);
+			 return A;
+		 }
+	}
+	 
+	 class ConvertCelsius{
+		 
+		 A = 1;
+		 {
+			 Fahrenheit = ( A * 9/5)+32;
+			 return A;
+		 }
+		 {
+			 kelvin =  A + 273.15
+			 return A;
+		 }
+		 
+	class ConvertKelvin{
+		
+		 A = 1;
+		 {
+			  Celsius = A -273.15;
+			  return A;
+			  
+			  Fahrenheit = ( A * 9/5)+32;
+				 return A;
+		}
+	 }
+}
+}
